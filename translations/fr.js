20566aa2ea7681ced67e85645bbcb010d01e106d
Vincent Helwig <vincent.helwig@gmail.com>
2015-11-19 09:35:27 +0100

    feat(translation): Update fr

--- translations/fr.js
+++ translations/fr.js
@@ -1,195 +1,390 @@
 {
 
+
+
 "playerHeading": "Media player",
+
+
 
 "faster": "Accélerer",
 
+
+
 "slower": "Ralentir",
+
+
 
 "play": "Lire",
 
+
+
 "pause": "Pause",
+
+
 
 "stop": "Stop",
 
+
+
 "rewind": "Reculer",
+
+
 
 "forward": "Avancer",
 
+
+
 "captions": "Sous-titres",
+
+
 
 "showCaptions": "Afficher sous-titres",
 
+
+
 "hideCaptions": "Masquer sous-titres",
+
+
 
 "captionsOff": "Sous-titres désactivés",
 
+
+
 "showTranscript": "Afficher transcription",
+
+
 
 "hideTranscript": "Masquer transcription",
 
-"turnOnDescriptions": "Audio-description activée",
 
-"turnOffDescriptions": "Audio-description désactivée",
 
-"turnOnCued": "LPC activée",
+"turnOnDescriptions": "Activer audio-description",
 
-"turnOffCued": "LPC désactivée",
+
+
+"turnOffDescriptions": "Désactiver audio-description",
+
+
+
+"turnOnCued": "Activer LPC",
+
+
+
+"turnOffCued": "Désactiver LPC",
+
+
 
 "chapters": "Chapitres",
 
+
+
 "language": "Langue",
+
+
 
 "sign": "Langue des signes",
 
-"showSign": "Afficher LSF",
 
-"hideSign": "Masquer LSF",
+
+"showSign": "Désactiver LSF",
+
+
+
+"hideSign": "Activer LSF",
+
+
 
 "mute": "Désactiver le son",
 
+
+
 "unmute": "Activer le son",
+
+
 
 "volume": "Volume",
 
+
+
 "volumeUp": "Volume +",
+
+
 
 "volumeDown": "Volume -",
 
+
+
 "preferences": "Préférences",
+
+
 
 "enterFullScreen": "Plein écran",
 
+
+
 "exitFullScreen": "Quitter le mode plein écran",
+
+
 
 "fullScreen": "Plein écran",
 
+
+
 "speed": "Vitesse",
+
+
 
 "audio": "audio",
 
+
+
 "video": "video",
+
+
 
 "or": "ou",
 
+
+
 "spacebar": "Barre d'espacement",
+
+
 
 "autoScroll": "Défilement automatique",
 
+
+
 "unknown": "Inconnu",
+
+
 
 "statusPlaying": "En lecture",
 
+
+
 "statusPaused": "En pause",
+
+
 
 "statusStopped": "Arrêté",
 
+
+
 "statusWaiting": "En attente",
+
+
 
 "statusBuffering": "En chargement",
 
+
+
 "statusUsingDesc": "En version décrite",
+
+
 
 "statusLoadingDesc": "Chargement de la version décrite",
 
+
+
 "statusUsingNoDesc": "En version non décrite",
+
+
 
 "statusLoadingNoDesc": "Chargement de la version non décrite",
 
+
+
 "statusLoadingNext": "Chargement de la piste suivante",
+
+
 
 "statusEnd": "Fin de piste",
 
+
+
 "selectedTrack": "Piste sélectionnée",
+
+
 
 "fallbackError1": "Désolé, votre navigateur ne peut lire cela",
 
+
+
 "fallbackError2": "les navigateurs suivants peuvent utiliser ce lecteur",
+
+
 
 "orHigher": "ou supérieur",
 
+
+
 "prefTitle": "Préférences",
+
+
 
 "prefIntro": "La sauvegarde de vos préférences nécessite l'utilisatisation de cookies.",
 
+
+
 "prefFeatures": "Caractéristiques",
+
+
 
 "prefKeys": "Touches de raccourcis",
 
+
+
 "prefAltKey": "Alt",
+
+
 
 "prefCtrlKey": "Control",
 
+
+
 "prefShiftKey": "Majuscule",
+
+
 
 "prefVideo": "Activer par défaut (un seul choix possible) :",
 
+
+
 "prefCaptions": "Sous-titres activés par défaut",
+
+
 
 "prefDefault": "Vidéo principale",
 
-"prefSignLanguage": "Vidéo principcale avec LSF",
 
-"prefDesc": "Vidéo principcale avec audiodescription",
 
-"prefCued": "Vidéo principcale avec LPC",
+"prefSignLanguage": "Vidéo principale avec LSF",
+
+
+
+"prefDesc": "Vidéo principale avec audiodescription",
+
+
+
+"prefCued": "Vidéo principale avec LPC",
+
+
 
 "prefClosedDesc": "Description par texte si disponible",
 
+
+
 "prefDescPause": "Pause à l'affichage du texte de description",
+
+
 
 "prefVisibleDesc": "Afficher le texte de description s'il est activé",
 
+
+
 "prefTranscript": "Transcription activée par défaut",
+
+
 
 "prefHighlight": "Mettre la transcription en surbrillance pendant que le media est lu",
 
+
+
 "prefTabbable": "Clavier actif sur la transcription",
+
+
 
 "prefSuccess": "Vos modifications ont été enregistrées.",
 
+
+
 "prefNoChange": "Vous n'avez effectué aucune modification.",
+
+
 
 "help": "Aide",
 
+
+
 "helpTitle": "Aide",
+
+
 
 "helpKeys": "Le lecteur peut être piloté depuis n'importe quel point de la page en utilisant les raccourcis claviers suivants :",
 
+
+
 "helpKeysDisclaimer": "Les touches de raccourcis (Maj, Alt, and Control) peuvent être définies dans les Préférences. Certaines combinaisons de touches de raccourcis peuvent être en conflit avec les touches utilisées par votre navigateur et/ou d'autres logiciels. Essayez plusieurs combinaisons de touches de raccourcis pour trouver celle qui fonctionne.",
+
+
 
 "save": "Enregistrer",
 
+
+
 "cancel": "Annuler",
+
+
 
 "ok": "Ok",
 
+
+
 "done": "Fait",
+
+
 
 "closeButtonLabel": "Fermer la fenêtre",
 
+
+
 "windowButtonLabel": "Options de la fenêtre",
+
+
 
 "windowMove": "Déplacer",
 
+
+
 "windowMoveAlert": "Glisser ou utiliser les flèches de déplacements pour déplacer la fenêtre; Entrée pour arrêter",
+
+
 
 "windowResize": "Redimensionner",
 
+
+
 "windowResizeHeading": "Redimensionner fenêtre d'interprétation",
+
+
 
 "windowResizeAlert": "La fenêtre a été redimensionnée.",
 
+
+
 "width": "largeur",
+
+
 
 "height": "Hauteur",
 
+
+
 "windowSendBack": "Passer derrière",
+
+
 
 "windowSendBackAlert": "Cette fenêtre est maintenant derrière d'autres objets de la page.",
 
+
+
 "windowBringTop": "Passer devant",
+
+
 
 "windowBringTopAlert": "Cette fenêtre est maintenant devant d'autres objets de la page."
 
+
+
 }
+
